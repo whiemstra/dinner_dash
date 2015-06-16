04be97b9012a0aaf90efb297adf6bb5ab8cf244f
Whitney Hiemstra <whitcolorado@gmail.com>
2015-06-16 14:53:21 -0600

    Items can be created and are displayed on index page

--- spec/features/item_spec.rb
+++ spec/features/item_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 describe 'view an item', type: :feature do
 
+  before(:each) do
+    Item.create(title: "Fried Snack", description: "fatty food", price: 10)
+  end
+
   it 'has items on items page' do
     visit items_path
     expect(page).to have_text("All Items")
+    expect(page).to have_text("Fried Snack")
+  end
+
+  it "clicking items title will go to item page" do
+    visit items_path
+    click_on('Fried Snack')
+
+    expect(current_path).to eq(item_path(Item.last))
+    expect(page).to have_text('Fried Snack')
+    expect(page).to have_text(10)
   end
 
 end
@@ -19,9 +33,10 @@ describe 'creating a new item', type: :feature do
     fill_in('Price', with: 3 )
     click_on "Create New Item"
 
-    #expect(current_path).to eq(items_path(@item))
+    expect(current_path).to eq(item_path(Item.last))
     expect(page).to have_text("Fried Oreos")
   end
+
 
 end
 

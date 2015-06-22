fcc31b15976ac48ddeba7fc39135a7c5c0dd8651
Whitney Hiemstra <whitcolorado@gmail.com>
2015-06-22 06:32:45 -0600

    User logout spec, one pending

--- spec/features/user/user_logout_spec.rb
+++ spec/features/user/user_logout_spec.rb
@@ -1,26 +1,47 @@
 require 'rails_helper'
 
-describe 'user log out', type: :feature do
+describe 'User Logout', type: :feature do
 
+  before(:each) do
+    @user = User.create(full_name: "Tom Petty",
+                        display_name: 'Tom',
+                        email: "petty@gmail.com",
+                        password: "freefallin")
+  end
 
-  # test "logged in user can log out" do
-  #   user = User.create(username: "rachel", password: "password")
-  #
-  #   visit login_path
-  #   fill_in "Username", with: user.username
-  #   fill_in "Password", with: "password"
-  #   click_button "Login"
-  #
-  #   assert page.has_content?("Welcome, rachel")
-  #
-  #   click_link "Logout"
-  #
-  #   refute page.has_content?("Welcome, rachel")
-  # end
+  it 'only shows a link if logged in and logs user out when clicked' do
+    visit root_path
+    click_on("Toggle navigation")
 
+    expect(page).to have_content("Sign Up/Login")
 
-  xit 'only shows a link if logged in'
-  xit 'logs a user out when clicked'
-  xit 'does not allow people to access pages when logged out'
+    click_on("Toggle navigation")
+    find('.dropdown-menu').click
+
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "freefallin"
+    click_button "Login"
+
+    expect(page).to have_content("All Items")
+    click_on("Toggle navigation")
+    expect(page).to have_content("Log out")
+
+    click_on("Toggle navigation")
+    click_on 'Log out'
+
+    expect(page).to have_content("You've successfully logged out.")
+    click_on("Toggle navigation")
+    expect(page).to have_content("Sign Up/Login")
+  end
+
+  xit 'does not allow people to access pages when logged out' do
+    visit root_path
+    click_on("Toggle navigation")
+
+    expect(page).to have_content("Sign Up/Login")
+
+    #past orders page....
+  end
+
 end
 

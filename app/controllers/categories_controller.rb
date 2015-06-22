2b8cb5a7c96726e6ab6ab9cf4d7111594d84a017
Whitney Hiemstra <whitcolorado@gmail.com>
2015-06-22 17:18:01 -0600

    Item availability for index and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,16 @@
 class CategoriesController < ApplicationController
+  # def show
+  #   @category = Category.find(params[:id])
+  #   @categories = Category.all
+  # end
+
   def show
     @category = Category.find(params[:id])
     @categories = Category.all
+    if current_user && current_admin?
+      @category_items = @category.items
+    else
+      @category_items = @category.items.available
+    end
   end
 end

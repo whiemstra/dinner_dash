e3cb1c4e8739771231d5f97d9ff35c052c78b7e6
vanegomez <vane.gomez@me.com>
2015-06-23 04:24:48 -0600

    add model category tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { Category.create(title: "Appetizers") }
+
+  it "is valid" do
+    expect(category).to be_valid
+  end
+
+  it "is invalid without a title" do
+    category.title = nil
+
+    expect(category).to_not be_valid
+  end
+
+  it "is invalid without a unique name" do
+    category
+    category2 = Category.create(title: "Appetizers")
+
+    expect(category2).to_not be_valid
+  end
+
+  it "responds to items" do
+    expect(category).to respond_to(:items)
+  end
 end
